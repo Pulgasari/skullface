8403055be840d8a8fb780bf17bb77e08e02563e3
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-06-24 00:48:13 +0200

    Update frontend.ts

--- src/plugins/router/frontend.ts
+++ src/plugins/router/frontend.ts
@@ -1,11 +1,13 @@
-// plugins/router/frontend.ts
-
 export function addRoute (route) {
   window.__skullface_router.addRoute(route);
 }
 
 export function navigate (path) {
   return window.__skullface_router.navigate(path);
+}
+
+export function navigateByName (name, params) {
+  return window.__skullface_router.navigateByName(name, params);
 }
 
 export function currentPath () {
