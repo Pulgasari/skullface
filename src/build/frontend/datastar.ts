b99523b9261b78e0eab86dd156dc60eadd1fc249
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-06-23 17:51:34 +0200

    Create datastar.ts

---
+++ src/build/frontend/datastar.ts
@@ -0,0 +1,10 @@
+export async function buildDatastar (ctx) {
+  await Deno.mkdir(ctx.paths.frontend, { recursive: true });
+
+  for await (const entry of Deno.readDir("frontend")) {
+    await Deno.copyFile(
+      `frontend/${entry.name}`,
+      `${ctx.paths.frontend}/${entry.name}`,
+    );
+  }
+}
