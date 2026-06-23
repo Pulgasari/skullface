29abcb2e0075bb54b3f8da132afc2d8d5283f4e0
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-06-23 14:15:50 +0200

    Create logger.ts

---
+++ src/core/logger.ts
@@ -0,0 +1,5 @@
+export const log = {
+  info  : (message: string) => console.log(`ℹ️ ${message}`),
+  warn  : (message: string) => console.log(`⚠️ ${message}`),
+  error : (message: string) => console.log(`❌ ${message}`),
+};
