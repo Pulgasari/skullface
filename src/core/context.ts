a6c0c520765b70ffdaf5e302f1f22b0e3d75752a
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-06-23 14:14:11 +0200

    Create context.ts

---
+++ src/core/context.ts
@@ -0,0 +1,19 @@
+import { loadConfig } from "./loadConfig.ts";
+import { loadPlugins } from "./plugins.ts";
+
+export async function createContext () {
+  const config = await loadConfig();
+
+  const ctx = {
+    config,
+    root: Deno.cwd(),
+    paths: {
+      dist     : "dist",
+      frontend : "dist/frontend",
+      backend  : "dist/backend",
+    },
+    plugins: await loadPlugins(config.plugins),
+  };
+
+  return ctx;
+}
