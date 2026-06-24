2ab62f3420ec9a4dc4e1310de3edaf6a09bc8be5
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-06-24 03:25:20 +0200

    Update create.ts

--- src/cli/commands/create.ts
+++ src/cli/commands/create.ts
@@ -1,38 +1,36 @@
 // cli/commands/create.ts
 
-import { ask } from "../utils/ask.ts";
-import { select } from "../utils/select.ts";
-import { multiselect } from "../utils/multiselect.ts";
-import { TEMPLATE_REGISTRY } from "../utils/templates.ts";
-import { copyTemplate } from "../utils/copy.ts";
-import { applyVariables } from "../utils/variables.ts";
-import { enablePlugins } from "../utils/plugins.ts";
+import {
+  ask, multiselect, select,
+  TEMPLATE_REGISTRY, copyTemplate,
+  applyVariables, enablePlugins
+} from './../utils.js';
 
-export async function createCommand() {
+export async function createCommand () {
   console.log("Skullface Create Wizard\n");
 
   const slug = await ask("App ID / Slug:");
   const name = (await ask("App Name (Enter = same as slug):")) || slug;
 
   const framework = await select("Choose frontend:", [
-    "vanilla",
+    "datastar",
+    "htmx",
     "preact",
     "react",
     "svelte",
-    "htmx",
-    "datastar"
+    "vanilla"
   ]);
 
   const plugins = await multiselect("Select plugins:", [
-    "fs",
-    "store",
-    "sqlite",
-    "logger",
-    "router",
-    "hotkeys",
-    "notifications",
     "dialogs",
-    "launch"
+    "fs",
+    "hotkeys",
+    "launch",
+    "logger",
+    "notifications",
+    "router",
+    "sqlite",
+    "store",
   ]);
 
   const targetDir = `./${slug}`;
@@ -46,4 +44,4 @@ export async function createCommand() {
   console.log("\nDone!");
   console.log(`cd ${slug}`);
   console.log("skullface dev");
-    }
+}
