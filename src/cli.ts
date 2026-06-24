42f512c403a093cb66eed951f722ab05f7f03965
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-06-24 23:44:12 +0200

    Delete src/cli.ts

--- src/cli.ts
+++
@@ -1,9 +0,0 @@
-import { parseArgs } from "@std/cli";
-import { printBranding } from "./core/branding.ts";
-import { runCommand } from "./core/runner.ts";
-
-const { _: raw, ...flags } = parseArgs(Deno.args);
-const [command, ...args] = raw;
-
-printBranding();
-await runCommand(command, args, flags);
