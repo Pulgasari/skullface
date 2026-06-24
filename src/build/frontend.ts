dd21274767ac48a6c7a775464fe09fa96905f1e5
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-06-24 23:56:22 +0200

    Update frontend.ts

--- src/build/frontend.ts
+++ src/build/frontend.ts
@@ -1,7 +1,4 @@
-import { buildDatastar } from "./frontend/datastar.ts";
-import { buildHTMX }     from "./frontend/htmx.ts";
-import { buildPreact }   from "./frontend/preact.ts";
-
+// build/frontend.ts
 
 export async function buildFrontend (ctx) {
   switch (ctx.config.template) {
@@ -10,4 +7,34 @@ export async function buildFrontend (ctx) {
     case "preact"   : return buildPreact(ctx);
     default         : throw new Error("Unknown template: " + ctx.config.template);
   }
+}
+
+async function buildDatastar (ctx) {
+  await Deno.mkdir(ctx.paths.frontend, { recursive: true });
+
+  for await (const entry of Deno.readDir("frontend")) {
+    await Deno.copyFile(
+      `frontend/${entry.name}`,
+      `${ctx.paths.frontend}/${entry.name}`,
+    );
+  }
+}
+
+async function buildHTMX (ctx) {
+  await Deno.mkdir(ctx.paths.frontend, { recursive: true });
+
+  for await (const entry of Deno.readDir("frontend")) {
+    await Deno.copyFile(
+      `frontend/${entry.name}`,
+      `${ctx.paths.frontend}/${entry.name}`,
+    );
+  }
+}
+
+async function buildPreact (ctx) {
+  const p = new Deno.Command("deno", {
+    args: ["task", "build:frontend"],
+  }).spawn();
+
+  await p.status;
 }
