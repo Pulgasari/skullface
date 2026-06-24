2a101fc1872e952c6d54061b9e1c56906fed1a15
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-06-25 00:19:09 +0200

    Refactor createCommand to use dynamic templates

--- src/cli/commands/create.ts
+++ src/cli/commands/create.ts
@@ -6,22 +6,9 @@ import {
   applyVariables, enablePlugins
 } from './../utils.js';
 
-export default async function createCommand () {
-  console.log("Skullface Create Wizard\n");
-
-  const slug = await ask("App ID / Slug:");
-  const name = (await ask("App Name (Enter = same as slug):")) || slug;
-
-  const framework = await select("Choose frontend:", [
-    "datastar",
-    "htmx",
-    "preact",
-    "react",
-    "svelte",
-    "vanilla"
-  ]);
-
-  const plugins = await multiselect("Select plugins:", [
+//let pluginKeys = Object.keys(PLUGIN_REGISTRY);
+let templateKeys = Object.keys(TEMPLATE_REGISTRY);
+let pluginKeys = [
     "dialogs",
     "fs",
     "hotkeys",
@@ -31,8 +18,15 @@ export default async function createCommand () {
     "router",
     "sqlite",
     "store",
-  ]);
+];
 
+export default async function createCommand () {
+  console.log("Skullface Create Wizard\n");
+
+  const slug      = await ask("App ID / Slug:");
+  const name      = (await ask("App Name (Enter = same as slug):")) || slug;
+  const framework = (await select("Choose frontend:", templateKeys)) || 'vanilla';
+  const plugins   = await multiselect("Select plugins:", pluginKeys);
   const targetDir = `./${slug}`;
 
   console.log("\nCreating project:", targetDir);
