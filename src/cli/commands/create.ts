1bbfab1a9b4a6ae345b25e6371234634e0c21e69
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-06-24 03:15:00 +0200

    Update create.ts

--- src/cli/commands/create.ts
+++ src/cli/commands/create.ts
@@ -1,15 +1,49 @@
-import { templates } from "../core/config.ts";
-import { log }       from "../core/logger.ts";
+// cli/commands/create.ts
 
-export default async function create([name]: string[]) {
-  if (!name) {
-    log.error("Missing project name.");
-    return;
-  }
+import { ask } from "../utils/ask.ts";
+import { select } from "../utils/select.ts";
+import { multiselect } from "../utils/multiselect.ts";
+import { TEMPLATE_REGISTRY } from "../utils/templates.ts";
+import { copyTemplate } from "../utils/copy.ts";
+import { applyVariables } from "../utils/variables.ts";
+import { enablePlugins } from "../utils/plugins.ts";
 
-  const template = templates["preact"];
-  await Deno.mkdir(name, { recursive: true });
+export async function createCommand() {
+  console.log("Skullface Create Wizard\n");
 
-  await Deno.copyFile(`${template}/project.json`, `${name}/project.json`);
-  log.info(`Created new Skullface project: ${name}`);
-}
+  const slug = await ask("App ID / Slug:");
+  const name = (await ask("App Name (Enter = same as slug):")) || slug;
+
+  const framework = await select("Choose frontend:", [
+    "vanilla",
+    "preact",
+    "react",
+    "svelte",
+    "htmx",
+    "datastar"
+  ]);
+
+  const plugins = await multiselect("Select plugins:", [
+    "fs",
+    "store",
+    "sqlite",
+    "logger",
+    "router",
+    "hotkeys",
+    "notifications",
+    "dialogs",
+    "launch"
+  ]);
+
+  const targetDir = `./${slug}`;
+
+  console.log("\nCreating project:", targetDir);
+
+  await copyTemplate(framework, targetDir);
+  await applyVariables({ slug, name }, targetDir);
+  await enablePlugins(plugins, targetDir);
+
+  console.log("\nDone!");
+  console.log(`cd ${slug}`);
+  console.log("skullface dev");
+    }
