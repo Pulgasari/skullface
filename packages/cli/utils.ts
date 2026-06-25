d86f4c165d21899079fd6620f86ca1f36b8f83ff
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-06-25 08:04:40 +0200

    Create utils.ts

---
+++ packages/cli/utils.ts
@@ -0,0 +1,5 @@
+// packages/cli/utils.ts
+
+export const DEFAULT_TEMPLATE   = 'vanilla';
+export const REPO_ZIP_URL       = `https://github.com/pulgasari/skullface/archive/refs/heads/main.zip`;
+export const TEMPLATE_PATH_PART = `skullface-main/templates/vanilla/`;
