b3d6bd19ff9734a41b5aa80d45297f26c558202c
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-06-25 23:49:34 +0200

    Update utils.ts

--- packages/cli/utils.ts
+++ packages/cli/utils.ts
@@ -8,22 +8,6 @@ export const DEFAULT_TEMPLATE   = 'vanilla';
 export const REPO_ZIP_URL       = `https://github.com/pulgasari/skullface/archive/refs/heads/main.zip`;
 export const TEMPLATE_PATH_PART = `skullface-main/templates/vanilla/`;
 
-// Templates
-
-export async function copyTemplate (name: string, targetDir: string) {
-  const sourceDir = templates[name];
-  await Deno.mkdir(targetDir, { recursive: true });
-
-  for await (const entry of Deno.readDir(sourceDir)) {
-    const from = `${sourceDir}/${entry.name}`;
-    const to   = `${targetDir}/${entry.name}`;
-
-    entry.isDirectory
-    ? await copyTemplate(`${name}/${entry.name}`, to)
-    : await Deno.copyFile(from, to);
-  }
-}
-
 // Plugins
 
 export async function enablePlugins (plugins: string[], dir: string) {
@@ -41,7 +25,24 @@ export async function enablePlugins (plugins: string[], dir: string) {
   await Deno.writeTextFile(configPath, JSON.stringify(config, null, 2));
 }
 
-// Variables
+// Templates
+
+export async function copyTemplate (
+  name      : string, 
+  targetDir : string
+) {
+  const sourceDir = templates[name];
+  await Deno.mkdir(targetDir, { recursive: true });
+
+  for await (const entry of Deno.readDir(sourceDir)) {
+    const from = `${sourceDir}/${entry.name}`;
+    const to   = `${targetDir}/${entry.name}`;
+
+    entry.isDirectory
+    ? await copyTemplate(`${name}/${entry.name}`, to)
+    : await Deno.copyFile(from, to);
+  }
+}
 
 export async function applyVariables (
   vars : Record<string, string>, 
@@ -69,3 +70,9 @@ export async function applyVariables (
     await Deno.writeTextFile(path, text);
   }
 }
+
+
+
+
+
+
