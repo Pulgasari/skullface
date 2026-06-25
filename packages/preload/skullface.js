d1cfe71d7fe77e74ae8cfbc03f1ddc6fb5187109
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-06-25 10:40:02 +0200

    Update skullface.js

--- packages/preload/skullface.js
+++ packages/preload/skullface.js
@@ -1,1 +1,11 @@
 // packages/preload/skullface.js
+// (Will be injected into webview.)
+
+(() => {
+  window.__skullface__ = {
+    rpc: async (command, data) => {
+      const payload = JSON.stringify({ command, data });
+      return window.ipc.postMessage(payload);
+    }
+  };
+})();
