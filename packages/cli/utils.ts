def26b25ed9c6ad18b5358203a1a674e86739409
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-06-26 21:45:50 +0200

    Update utils.ts

--- packages/cli/utils.ts
+++ packages/cli/utils.ts
@@ -1,6 +1,10 @@
 // packages/cli/utils.ts
 
 import { templates } from './registry.js';
+import wizard        from './wizard.ts';
+import { JSZip } from "https://deno.land/x/jszip@0.11.0/mod.ts";
+import { join } from "https://deno.land/std@0.224.0/path/mod.ts";
+
 
 // Constants
 
@@ -27,7 +31,54 @@ export async function enablePlugins (plugins: string[], dir: string) {
 
 // Templates
 
-export async function copyTemplate (
+export async function copyTemplate ({
+  dir  : string,
+  name : string = "vanilla-ts"
+}) {
+  let targetDir = dir;
+  try {
+    const repoZipUrl = REPO_ZIP_URL;
+    wizard.print("Download Template ...");
+    const response = await fetch(repoZipUrl);
+    if (!response.ok) throw new Error(`Download failed: ${response.statusText}`);
+    
+    const expectedPathPart = `skullface-main/templates/${name}/`;
+    const zipData   = await response.arrayBuffer();
+    const zip       = new JSZip();
+    const loadedZip = await zip.loadAsync(zipData);
+    
+    await Deno.mkdir(targetDir, { recursive: true });
+
+    wizard.print("Unzip Project Files ...);
+  
+    for (const [filename, fileObj] of Object.entries(loadedZip.files)) {
+      if (filename.includes(expectedPathPart) && !fileObj.dir) {
+          
+        // "skullface-main/templates/vanilla-ts/src-frontend/index.html" 
+        // -> "src-frontend/index.html"
+        const relativePath   = filename.replace(expectedPathPart, '');
+        const fullOutputPath = join(targetDir, relativePath);
+  
+        // Ensure Directory Structure
+        const fileDir = join(fullOutputPath, "..");
+        await Deno.mkdir(fileDir, { recursive: true });
+          
+        // Read and Write File as Uint8Array (Bytes)
+        const content = await fileObj.async("uint8array");
+        await Deno.writeFile(fullOutputPath, content);
+          
+        wizard.print(`   + ${relativePath}`);
+      }
+    }
+
+    wizard.print(`Done!`);
+  } 
+  catch (error) {
+    console.error("Error on Create:", error.message);
+  }
+}
+
+export async function copyTemplateOld (
   name      : string, 
   targetDir : string
 ) {
