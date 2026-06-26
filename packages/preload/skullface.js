e6d70c4270f0c54465c7d135d9a309b55b0e0eb5
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-06-26 22:04:40 +0200

    Update skullface.js

--- packages/preload/skullface.js
+++ packages/preload/skullface.js
@@ -1,8 +1,11 @@
 // packages/preload/skullface.js
 // (Will be injected into webview.)
 
+import paths from './paths.ts';
+
 (() => {
   window.__skullface__ = {
+    paths,
     rpc: async (command, data) => {
       const payload = JSON.stringify({ command, data });
       return window.ipc.postMessage(payload);
