cc73deeb16545dcf3e68340fa48fe7840bdaf149
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-06-27 01:26:26 +0200

    Create mod.ts

---
+++ packages/core/mod.ts
@@ -0,0 +1,8 @@
+// jsr:@skullface/core/mod.ts
+
+import { createContext, loadConfig, log, loadPlugins } from './core.ts';
+
+export createContext;
+export loadConfig;
+export log;
+export loadPlugins;
