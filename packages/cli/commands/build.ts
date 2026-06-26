2d99ce790fa394eb3cb914728b248ebea58868ad
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-06-27 01:01:31 +0200

    Update build.ts

--- packages/cli/commands/build.ts
+++ packages/cli/commands/build.ts
@@ -1,14 +1,34 @@
 // packages/cli/commands/build.ts
 
-import { createContext } from "../core/context.ts";
-import { prepare }       from "../build/prepare.ts";
-import { buildFrontend } from "../build/frontend.ts";
-import { buildBackend }  from "../build/backend.ts";
-import { compile }       from "../build/compile.ts";
-import { buildAppImage } from "../build/appimage.ts";
-import { log }           from "../core/logger.ts";
+// Import: Deno
+import { join } from "https://deno.land/std@0.224.0/path/mod.ts";
 
+// Import: jsr:@skullface/core
+import { createContext } from "./../core/context.ts";
+import { log }           from "./../core/logger.ts";
+
+// Import: jsr:@skullface/cli (self)
+import { prepare }       from "./../build/prepare.ts";
+import { buildFrontend } from "./../build/frontend.ts";
+import { buildBackend }  from "./../build/backend.ts";
+import { compile }       from "./../build/compile.ts";
+import { buildAppImage } from "./../build/appimage.ts";
+
+// Usage: skullface build
 export default async function build () {
+
+  const projectPath  = Deno.cwd(); 
+  const configPath   = join(projectPath, "skullface.config.js");
+  const backendPath  = join(projectPath, "src-backend");
+  const frontendPath = join(projectPath, "src-frontend");
+
+  try       { await Deno.stat(configPath); }
+  catch (e) { wizard.print('SkullfaceConfig missing.'); }
+
+  // dynamic import
+  const config      = await import(`file://${configPath}`);
+  const appName     = config.app?.name     || "SkullfaceApp";
+  const windowWidth = config.window?.width || 800;
 
   // AppImage
   if (args.includes("--target") && args.includes("appimage")) {
