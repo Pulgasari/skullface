10dba79cd1a264f77f534f3d4dc8368124efa779
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-06-26 22:01:38 +0200

    Update create.ts

--- packages/cli/commands/create.ts
+++ packages/cli/commands/create.ts
@@ -1,19 +1,16 @@
 // packages/cli/commands/create.ts
 
-import wizard from './../wizard.ts';
-import { plugins, templates } from './../registry.js';
+import registry from './../registry.js';
+import wizard   from './../wizard.ts';
 import { applyVariables, copyTemplate, enablePlugins } from './../utils.js';
-
-let   pluginKeys = Object.keys(plugins);
-let templateKeys = Object.keys(templates);
 
 export default async function createCommand () {
   wizard.print("Skullface Create Wizard");
 
   const slug      =  await wizard.ask("App ID / Slug:");
   const name      = (await wizard.ask("App Name (Enter = same as slug):")) || slug;
-  const framework = (await wizard.select("Choose frontend:", templateKeys)) || 'vanilla';
-  const plugins   =  await wizard.multiselect("Select plugins:", pluginKeys);
+  const framework = (await wizard.select("Choose frontend:", registry.templates)) || 'vanilla';
+  const plugins   =  await wizard.multiselect("Select plugins:", registry.plugins);
   const targetDir = `./${slug}`;
 
   wizard.print("Creating project:", targetDir);
