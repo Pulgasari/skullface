ff2d8ecf9c2a86a05c084304384e928dfdc12513
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-06-27 10:07:34 +0200

    Update mod.ts

--- packages/plugins/clipboard/mod.ts
+++ packages/plugins/clipboard/mod.ts
@@ -11,3 +11,25 @@ export default {
     }
   },
 };
+
+// :::::: INTERFACE
+
+// packages/plugins/clipboard/types.ts
+
+export interface ClipboardAPI {
+  copy     (text: string) : Promise<void>;
+  copyHTML (html: string) : Promise<void>;
+  copyJSON (obj: any)     : Promise<void>;
+  paste    ()             : Promise<string>;
+}
+
+declare global {
+  interface Window {
+    skullface: {
+      clipboard: ClipboardAPI;
+    };
+  }
+}
+
+// additional usage: import { clipboard } from "plugins/clipboard";
+export const clipboard: ClipboardAPI = (window as any).skullface?.clipboard;
