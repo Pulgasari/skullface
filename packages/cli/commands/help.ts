ec7ec593e2c9f9c577e7e1ab92627ff854dfe2e4
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-06-28 01:43:24 +0200

    Update help.ts

--- packages/cli/commands/help.ts
+++ packages/cli/commands/help.ts
@@ -1,12 +1,14 @@
 // @aufbau/cli/commands/help.ts
 
-import { commands } from '@/types';
+import { COMMANDS } from '@/types';
+import wizard       from '@/wizard';
+
+const buildCommandOptions : string[] = [
+  '-t, --target <platform>  -> Erzwingt den Build für eine bestimmte Plattform (mac, windows, linux)',
+];
 
 export default function helpCommand () {
-  console.log("💀 Skullface CLI");
-  console.log("\nVerfügbare Befehle:");
-  console.log("  create   -> Erstellt ein neues Skullface Projekt via Wizard");
-  console.log("  build    -> Kompiliert das Projekt basierend auf der Config");
-  console.log("\nOptionen für 'build':");
-  console.log("  -t, --target <platform>  -> Erzwingt den Build für eine bestimmte Plattform (mac, windows, linux)");
+  wizard.print("💀 Skullface CLI");
+  wizard.list(COMMANDS, { title: 'Commands:' });
+  wizard.list(buildCommandOptions, { title: "Options for 'build':" });
 }
