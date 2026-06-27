146950a4a616a500b33c0c634434061d81d8660a
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-06-27 06:13:44 +0200

    Delete packages/plugins/sqlite/frontend.ts

--- packages/plugins/sqlite/frontend.ts
+++
@@ -1,7 +0,0 @@
-// plugins/sqlite/frontend.ts
-
-export const execute = (stmt, vals) =>
-  window.__skullface_sqlite.execute(stmt, vals);
-
-export const query = (stmt, vals) =>
-  window.__skullface_sqlite.query(stmt, vals);
