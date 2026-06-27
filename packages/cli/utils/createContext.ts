4974d0e3776766a8e768b9039c5b73ee0a6f9679
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-06-27 02:23:18 +0200

    Create createContext.ts

---
+++ packages/cli/utils/createContext.ts
@@ -0,0 +1,20 @@
+// @skullface/cli/utils/createContext.ts
+
+import { loadConfig } from '@/utils';
+
+export async function createContext () {
+  const config = await loadConfig();
+
+  const ctx = {
+    config,
+    root: Deno.cwd(),
+    paths: {
+      dist     : "dist",
+      frontend : "dist/frontend",
+      backend  : "dist/backend",
+    },
+    plugins: await loadPlugins(config.plugins),
+  };
+
+  return ctx;
+}
