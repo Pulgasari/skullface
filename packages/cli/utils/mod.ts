fb45cc06bfc04b165cf6efb0cdbdb2d13c3ded68
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-06-27 02:08:12 +0200

    Update mod.ts

--- packages/cli/utils/mod.ts
+++ packages/cli/utils/mod.ts
@@ -1,3 +1,10 @@
 // @skullface/cli/utils/mod.ts
 
-export { default as copyTemplate } from './copyTemplate.ts';
+// Constants
+export const DEFAULT_TEMPLATE   = 'vanilla';
+export const REPO_ZIP_URL       = `https://github.com/pulgasari/skullface/archive/refs/heads/main.zip`;
+export const TEMPLATE_PATH_PART = `skullface-main/templates/vanilla/`;
+
+// Methods
+export { default as applyTemplateVars } from './applyTemplateVars.ts';
+export { default as copyTemplate      } from './copyTemplate.ts';
