8eb6ac61a8f4a86938a5ba37c4e4dcaa44156299
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-06-27 14:02:30 +0200

    Update windows.ts

--- packages/cli/packer/windows.ts
+++ packages/cli/packer/windows.ts
@@ -4,10 +4,11 @@ import wizard from '@/wizard';
 import { Packer } from "./mod.ts";
 
 export class WindowsPacker implements Packer {
-  async pack (binaryPath: string, projectRoot: string): Promise<void> {
+  async pack (binaryPath: string, projectRoot: string, appMeta: { name: string; slug: string; options: any }): Promise<void> {
     console.log("[Packer:Windows] Bereite Windows Release-Ordner vor...");
 
-    const appName   = "SkullfaceApp";
+    const appName   = appMeta.name;
+    const appSlug   = appMeta.slug;
     const outputDir = `${projectRoot}/dist-native/windows`;
 
     // 1. Ausgabe-Ordner erstellen
