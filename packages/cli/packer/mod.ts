7e798c1b6589830b9ff7d47175efb8eb65a7cf72
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-06-27 20:12:54 +0200

    Update mod.ts

--- packages/cli/packer/mod.ts
+++ packages/cli/packer/mod.ts
@@ -1,5 +1,6 @@
 // @skullface/cli/src/packer/mod.ts
 
+import { Platform } from '@/types';
 import     { MacPacker } from   "./macos.ts";
 import { WindowsPacker } from "./windows.ts";
 import   { LinuxPacker } from   "./linux.ts";
@@ -8,11 +9,11 @@ export interface Packer {
   pack (binaryPath: string, projectRoot: string) : Promise<void>;
 }
 
-export function getPacker (target: "mac" | "windows" | "linux"): Packer {
+export function getPacker (target: Platform): Packer {
   switch (target) {
-    case "mac"     : return new     MacPacker();
-    case "windows" : return new WindowsPacker();
-    case "linux"   : return new   LinuxPacker();
-    default: throw new Error(`Unsupported packaging platform: ${target}`);
+    case 'linux'   : return new   LinuxPacker();
+    case 'mac'     : return new     MacPacker();
+    case 'windows' : return new WindowsPacker();
+    default        : throw new Error(`Unsupported packaging platform: ${target}`);
   }
 }
