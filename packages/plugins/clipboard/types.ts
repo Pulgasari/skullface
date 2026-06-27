b601e30aca21c39e86722f349f12280cf36fac19
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-06-27 10:03:47 +0200

    Delete packages/plugins/clipboard/types.ts

--- packages/plugins/clipboard/types.ts
+++
@@ -1,6 +0,0 @@
-export interface ClipboardAPI {
-  copy     (text: string) : Promise<void>;
-  copyHTML (html: string) : Promise<void>;
-  copyJSON (obj: any)     : Promise<void>;
-  paste()                 : Promise<string>;
-}
