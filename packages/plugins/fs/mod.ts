6a67939707fe3b8f2b47ed9705a3e9579772c1cc
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-06-27 06:25:54 +0200

    Update mod.ts

--- packages/plugins/fs/mod.ts
+++ packages/plugins/fs/mod.ts
@@ -11,3 +11,28 @@ export default {
     }
   },
 };
+
+// :::::: Interface
+
+export interface FileSystemAPI {
+  readText  (path: string)                : Promise<string>;
+  writeText (path: string, text: string)  : Promise<void>;
+  readJSON  (path: string)                : Promise<any>;
+  writeJSON (path: string, obj: any)      : Promise<void>;
+  exists    (path: string)                : Promise<boolean>;
+  copy      (src: string, dest: string)   : Promise<void>;
+  remove    (path: string)                : Promise<void>;
+  mkdir     (path: string)                : Promise<void>;
+  walk      (path: string, options?: any) : Promise<any[]>;
+}
+
+declare global {
+  interface Window {
+    skullface: {
+      fs: FileSystemAPI;
+    };
+  }
+}
+
+// to be used like: import { fs } from 'plugins/fs'
+// export const fs: FilesystemAPI = (window as any).skullface?.fs;
