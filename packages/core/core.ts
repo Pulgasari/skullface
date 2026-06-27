08dc4feb91f596109b917b17ffb3c55a99b7c4e1
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-06-27 02:33:16 +0200

    Update core.ts

--- packages/core/core.ts
+++ packages/core/core.ts
@@ -1,45 +1,5 @@
 // core.ts
 
-export async function createContext () {
-  const config = await loadConfig();
-
-  const ctx = {
-    config,
-    root: Deno.cwd(),
-    paths: {
-      dist     : "dist",
-      frontend : "dist/frontend",
-      backend  : "dist/backend",
-    },
-    plugins: await loadPlugins(config.plugins),
-  };
-
-  return ctx;
-}
-
-export async function getProjectPaths (dir: string) {
-  dir ??= Deno.cwd(); 
-  const backend  = join(dir, "src-backend");
-  const frontend = join(dir, "src-frontend");
-  
-  return { 
-    backend, 
-    frontend,
-  };
-}
-
-export async function loadConfig (dir: string) {
-  dir ??= Deno.cwd();
-  const configPath = `${dir}/skullface.config.js`;
-  
-  try       { await Deno.stat(configPath); }
-  catch (e) { console.error('SkullfaceConfig missing.'); }
-  
-  const mod = await import(configPath);
-  //const mod = await import(`file://${configPath}`)
-  return mod.default;
-}
-
 export const log = {
   info  : (message: string) => console.log(`ℹ️ ${message}`),
   warn  : (message: string) => console.log(`⚠️ ${message}`),
