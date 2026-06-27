d7ccce302277d4c0d49032a8d20151807cd50427
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-06-27 19:42:46 +0200

    Delete packages/cli/bundler/build directory

--- packages/cli/bundler/build/prepare.ts
+++
@@ -1,6 +0,0 @@
-export async function prepare (ctx) {
-  await Deno.remove("dist", { recursive: true }).catch(() => {});
-  await Deno.mkdir("dist", { recursive: true });
-
-  await ctx.plugins.runHook("onBuildStart", ctx);
-}
