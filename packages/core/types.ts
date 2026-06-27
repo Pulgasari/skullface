05b81dac96818a31cfff8a6ba0e3b5ef66d293ec
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-06-27 19:57:22 +0200

    Update types.ts

--- packages/core/types.ts
+++ packages/core/types.ts
@@ -1,8 +1,43 @@
 // @skullface/core/types.ts
 
+export const commands = {
+  'build',
+  'create',
+  'dev',
+  'doctor',
+  'plugin',
+  'template',
+} as const;
+
+export const plugins = [
+  "clipboard",
+  "dialogs",
+  "external",
+  "fs",
+  "hotkeys",
+  "logger",
+  "notifications",
+  "router",
+  "sqlite",
+  "store",
+] as const;
+
+export const templates = [
+  'datastar',
+  'htmx',
+  'preact',
+  'react',
+  'svelte',
+  'vanilla',
+  'vanilla-ts',
+] as const;
+
 // :::::: TYPES
 
+export type Command  = typeof commands[number];
 export type Platform = 'linux' | 'mac' | 'windows';
+export type Plugin   = typeof plugins[number];
+export type Template = typeof templates[number];
 
 // :::::: INTERFACES
 
