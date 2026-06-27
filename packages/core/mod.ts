4728151f70f562ff68c2ff0afe40629113171314
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-06-27 02:34:01 +0200

    Update mod.ts

--- packages/core/mod.ts
+++ packages/core/mod.ts
@@ -1,25 +1,21 @@
 // jsr:@skullface/core/mod.ts
 
-import { createContext, loadConfig, log, loadPlugins } from './core.ts';
+import { log, loadPlugins } from './core.ts';
 import { getPaths, joinPaths, ensurePathExists } from './paths.ts';
 import { createWindow } from './window.ts';
 
-export createContext;
 export createWindow;
 export ensurePathExists;
 export getPaths;
 export joinPaths;
-export loadConfig;
 export log;
 export loadPlugins;
 
 export default {
-  createContext,
   createWindow,
   ensurePathExists,
   getPaths,
   joinPaths,
-  loadConfig,
   log,
   loadPlugins,
 }
