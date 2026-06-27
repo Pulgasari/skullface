c34b9b7c509d332beb8199e8616706492eb04849
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-06-27 10:32:42 +0200

    Update mod.ts

--- packages/core/mod.ts
+++ packages/core/mod.ts
@@ -1,22 +1,35 @@
-// jsr:@skullface/core/mod.ts
+// @skullface/core/mod.ts
 
-import { log, loadPlugins } from './core.ts';
-import { getPaths, joinPaths, ensurePathExists } from './paths.ts';
-import { createWindow } from './window.ts';
+import { SkullfaceWindow } from "./window.ts";
 
-export createWindow;
-export ensurePathExists;
-export getPaths;
-export joinPaths;
-export log;
-export loadPlugins;
+// Plugins importieren
+import fsPlugin from "../plugins/fs/mod.ts";
+import sqlitePlugin from "../plugins/sqlite/mod.ts";
+import storePlugin from "../plugins/store/mod.ts";
+import clipboardPlugin from "../plugins/clipboard/mod.ts";
+import hotkeysPlugin from "../plugins/hotkeys/mod.ts";
 
-export default {
-  createWindow,
-  ensurePathExists,
-  getPaths,
-  joinPaths,
-  log,
-  loadPlugins,
-}
+// 1. Fenster initialisieren
+const appWindow = new SkullfaceWindow({
+  title: "Meine Skullface App",
+  url: "http://localhost:3000", // Dein Frontend Dev-Server oder dist/index.html
+  width: 1200,
+  height: 800
+});
 
+// 2. Plugins im Core anmelden
+appWindow.registerPlugin(fsPlugin.name, fsPlugin.api);
+appWindow.registerPlugin(sqlitePlugin.name, sqlitePlugin.api);
+appWindow.registerPlugin(storePlugin.name, storePlugin.api);
+appWindow.registerPlugin(clipboardPlugin.name, clipboardPlugin.api);
+appWindow.registerPlugin(hotkeysPlugin.name, hotkeysPlugin.api);
+
+// 3. Lifecycle-Hooks triggern
+fsPlugin.hooks.onInit();
+sqlitePlugin.hooks.onInit();
+storePlugin.hooks.onInit();
+clipboardPlugin.hooks.onInit();
+hotkeysPlugin.hooks.onInit();
+
+// 4. App starten!
+appWindow.run();
