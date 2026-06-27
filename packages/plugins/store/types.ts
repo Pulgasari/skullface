6ed5b32be4dbb06da51eafd28bf5b48735a44942
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-06-27 09:54:17 +0200

    Delete packages/plugins/store/types.ts

--- packages/plugins/store/types.ts
+++
@@ -1,9 +0,0 @@
-export interface Store {
-  load   (): Record<string, any>;
-  save   (): void;
-  get    (key: string): any;
-  set    (key: string, value: any): void;
-  delete (key: string): void;
-  clear  (): void;
-  all    (): Record<string, any>;
-}
