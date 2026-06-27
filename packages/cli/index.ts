9dde7f775c31737283b484ef4898125745b6a7ee
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-06-27 23:45:12 +0200

    Update index.ts

--- packages/cli/index.ts
+++ packages/cli/index.ts
@@ -1,28 +1,19 @@
 #!/usr/bin/env -S deno run -A
+// @skullface/cli/mod.ts
 
-import  buildCommand from "./commands/build.ts";
+import { parseArgs } from "@std/cli/parse-args";
+import { buildCommand } from "./commands/build.ts";
 import createCommand from "./commands/create.ts";
+import   helpCommand from "./commands/help.ts";
 
-const args = Deno.args;
-const cmd  = args[0];
+// Flags parsen (z. B. --target linux oder -t linux)
+const flags = parseArgs(Deno.args, {
+  string : ["target"],
+  alias  : { target: "t" },
+});
 
-if (cmd === 'build')  await buildCommand();
-if (cmd === 'create') await createCommand();
+const cmd = flags._[0];
 
-// if no legal command name is provided
-// list all legal commands
-if (!cmd) {
-  console.log("Skullface CLI");
-  console.log("Commands:");
-  console.log("  build    -> ...")
-  console.log("  create   -> Create a new Skullface project");
-}
-
-/*
-import { parseArgs } from "@std/cli";
-import { runCommand } from "./core/runner.ts";
-
-const { _: raw, ...flags } = parseArgs(Deno.args);
-const [command, ...args] = raw;
-await runCommand(command, args, flags);
-*/
+     if (cmd === 'build')  await  buildCommand({ target: flags.target });
+else if (cmd === 'create') await createCommand();
+else                       await   helpCommand();
