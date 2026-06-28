22c261e48f592f274ec19171912f779c1426e944
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-06-28 18:54:59 +0200

    Update mod.ts

--- packages/cli/mod.ts
+++ packages/cli/mod.ts
@@ -1,12 +1,13 @@
 #!/usr/bin/env -S deno run -A
 // @skullface/cli/mod.ts
 
-import { parseArgs } from "@std/cli/parse-args";
-import  buildCommand from "./commands/build.ts";
-import createCommand from "./commands/create.ts";
-import   helpCommand from "./commands/help.ts";
+import { COMMANDS }  from '@/types';
+import { parseArgs } from '@std/cli/parse-args';
+import  buildCommand from './commands/build.ts';
+import createCommand from './commands/create.ts';
+import    devCommand from './commands/dev.ts';
+import   helpCommand from './commands/help.ts';
 import pluginCommand from './commands/plugin.ts';
-import { COMMANDS } from '@/types';
 
 // Flags parsen (z. B. --target linux oder -t linux)
 const flags = parseArgs(Deno.args, {
@@ -17,8 +18,9 @@ const flags = parseArgs(Deno.args, {
 const cmd     = flags._[0];
 const subArgs = flags._.slice(1).map(String); // captures everything after 'plugin'
 
-
-     if (cmd === 'build')  await  buildCommand({ target: flags.target });
+     if (cmd ===  'build') await  buildCommand({ target: flags.target });
 else if (cmd === 'create') await createCommand();
+else if (cmd ===    'dev') await    devCommand();
+else if (cmd ===   'help') await   helpCommand();
 else if (cmd === 'plugin') await pluginCommand(subArgs);
 else                       await   helpCommand();
