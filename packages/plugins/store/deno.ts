99dca26f1ffea9e0c2506e975a14471968a42043
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-06-29 01:27:02 +0200

    Update deno.ts

--- packages/plugins/store/deno.ts
+++ packages/plugins/store/deno.ts
@@ -32,81 +32,85 @@ function ensureCache (store: string) {
 
 // :::::: API (for IPC)
 
-export async function load (store: string): Promise<Record<string, any>> {
-  const data = readFromDisk(store);
-  cache.set(store, data);
-  return data;
-}
+const api = {
+  
+  async load (store: string): Promise<Record<string, any>> {
+    const data = readFromDisk(store);
+    cache.set(store, data);
+    return data;
+  },
+  
+  async save (store: string): Promise<void> {
+    const data = cache.get(store) || {};
+    writeToDisk(store, data);
+  },
 
-export async function save (store: string): Promise<void> {
-  const data = cache.get(store) || {};
-  writeToDisk(store, data);
-}
+  async get (store: string, key: string): Promise<any> {
+    ensureCache(store);
+    return cache.get(store)![key];
+  },
 
-export async function get (store: string, key: string): Promise<any> {
-  ensureCache(store);
-  return cache.get(store)![key];
-}
+  async set (store: string, key: string, value: any): Promise<void> {
+    ensureCache(store);
+    cache.get(store)![key] = value;
+    notify(store, key, value);
+  },
 
-export async function set (store: string, key: string, value: any): Promise<void> {
-  ensureCache(store);
-  cache.get(store)![key] = value;
-  notify(store, key, value);
-}
+  async remove (store: string, key: string): Promise<void> {
+    ensureCache(store);
+    delete cache.get(store)![key];
+    notify(store, key, undefined);
+  },
 
-export async function remove (store: string, key: string): Promise<void> {
-  ensureCache(store);
-  delete cache.get(store)![key];
-  notify(store, key, undefined);
-}
+  async clear (store: string): Promise<void> {
+    cache.set(store, {});
+  },
 
-export async function clear (store: string): Promise<void> {
-  cache.set(store, {});
-}
+  async all (store: string): Promise<Record<string, any>> {
+    ensureCache(store);
+    return { ...cache.get(store) };
+  },
 
-export async function all (store: string): Promise<Record<string, any>> {
-  ensureCache(store);
-  return { ...cache.get(store) };
-}
+  async entries (store: string): Promise<[string, any][]> {
+    ensureCache(store);
+    return Object.entries(cache.get(store)!);
+  },
+  
+  async keys (store: string): Promise<string[]> {
+    ensureCache(store);
+    return Object.keys(cache.get(store)!);
+  },
 
-export async function entries (store: string): Promise<[string, any][]> {
-  ensureCache(store);
-  return Object.entries(cache.get(store)!);
-}
+  async values (store: string): Promise<any[]> {
+    ensureCache(store);
+    return Object.values(cache.get(store)!);
+  },
+  
+  async has (store: string, key: string): Promise<boolean> {
+    ensureCache(store);
+    return Object.prototype.hasOwnProperty.call(cache.get(store)!, key);
+  },
 
-export async function keys (store: string): Promise<string[]> {
-  ensureCache(store);
-  return Object.keys(cache.get(store)!);
-}
+  async size (store: string): Promise<number> {
+    ensureCache(store);
+    return Object.keys(cache.get(store)!).length;
+  },
 
-export async function values (store: string): Promise<any[]> {
-  ensureCache(store);
-  return Object.values(cache.get(store)!);
-}
-
-export async function has (store: string, key: string): Promise<boolean> {
-  ensureCache(store);
-  return Object.prototype.hasOwnProperty.call(cache.get(store)!, key);
-}
-
-export async function size (store: string): Promise<number> {
-  ensureCache(store);
-  return Object.keys(cache.get(store)!).length;
-}
-
-export async function update (store: string, data: Record<string, any>): Promise<void> {
-  ensureCache(store);
-  const obj = cache.get(store)!;
-
-  for (const [key, value] of Object.entries(data)) {
-    if (value === undefined) {
-      delete obj[key];
-      notify(store, key, undefined);
-    } else {
-      obj[key] = value;
-      notify(store, key, value);
+  async update (store: string, data: Record<string, any>): Promise<void> {
+    ensureCache(store);
+    const obj = cache.get(store)!;
+  
+    for (const [key, value] of Object.entries(data)) {
+      if (value === undefined) {
+        delete obj[key];
+        notify(store, key, undefined);
+      } else {
+        obj[key] = value;
+        notify(store, key, value);
+      }
     }
-  }
+  },
+
 }
 
 // :::::: WATCH
@@ -118,8 +122,20 @@ function notify (store: string, key: string, value: any) {
   if (set) for (const fn of set) fn(key, value);
 }
 
-export function watch (store: string, fn: (key: string, value: any) => void) {
+api.watch = (store: string, fn: (key: string, value: any) => void) => {
   if (!watchers.has(store)) watchers.set(store, new Set());
   watchers.get(store)!.add(fn);
   return () => watchers.get(store)!.delete(fn); // optional: unsubscribe function
 }
+
+// :::::: EXPORT
+
+export default {
+  api,
+  name  : 'store',
+  hooks : {
+    onInit() {
+      console.log('[store] plugin successfully loaded.');
+    }
+  },
+};
