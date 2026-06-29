f59d89462b04c554653ba981be5871ffeca04028
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-06-29 04:05:03 +0200

    Update frontend.ts

--- packages/plugins/dialogs/frontend.ts
+++ packages/plugins/dialogs/frontend.ts
@@ -1,10 +1,38 @@
-// plugin/dialogs/frontend.ts
+// @skullface/plugins/dialogs/frontend.ts
 
-export const pickFile         = (o) => window.__skullface_dialogs.pickFile(o);
-export const pickFiles        = (o) => window.__skullface_dialogs.pickFiles(o);
-export const pickFolder       = (o) => window.__skullface_dialogs.pickFolder(o);
-export const pickSaveLocation = (o) => window.__skullface_dialogs.pickSaveLocation(o);
+export interface FileFilter {
+  extensions : string[];
+  name       : string;
+  
+}
 
-export const showMessage = (o) => window.__skullface_dialogs.showMessage(o);
-export const showConfirm = (o) => window.__skullface_dialogs.showConfirm(o);
-export const showError   = (o) => window.__skullface_dialogs.showError(o);
+export interface MessageDialogOptions {
+  body   : string;
+  title ?: string;
+}
+
+export interface OpenDialogOptions {
+  defaultPath ?: string;
+  filters     ?: FileFilter[];
+}
+
+export interface DialogsAPI {
+  pickFile         (options?: OpenDialogOptions)    : Promise<string>;
+  pickFiles        (options?: OpenDialogOptions)    : Promise<string[]>;
+  pickFolder       (options?: OpenDialogOptions)    : Promise<string>;
+  pickSaveLocation (options?: OpenDialogOptions)    : Promise<string>;
+  showMessage      (options : MessageDialogOptions) : Promise<void>;
+  showConfirm      (options : MessageDialogOptions) : Promise<boolean>;
+  showError        (options : MessageDialogOptions) : Promise<void>;
+}
+
+declare global {
+  interface Window {
+    skullface: {
+      dialogs: DialogsAPI;
+    };
+  }
+}
+
+// Global Shortcut Export
+export const dialogs: DialogsAPI = (window as any).skullface?.dialogs;
