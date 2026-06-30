44b38e6242bc5712c7ab6b2741812af00e855c03
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-06-30 18:22:53 +0200

    Update defaults.js

--- packages/core/defaults.js
+++ packages/core/defaults.js
@@ -2,8 +2,14 @@
 
 // :::::: CONSTANTS
 
-export const DEFAULT_APP_NAME      = 'SkullfaceApp';
+export const DEFAULT_APP_ICON         = 'src-assets/icon.svg';
+export const DEFAULT_APP_NAME         = 'SkullfaceApp';
+export const DEFAULT_APP_DIR_ASSETS   = 'src-assets';
+export const DEFAULT_APP_DIR_BACKEND  = 'src-backend',
+export const DEFAULT_APP_DIR_FRONTEND = 'src-frontend',
+
 export const DEFAULT_TEMPLATE      = 'vanilla';
+
 export const DEFAULT_WINDOW_TITLE  = DEFAULT_APP_NAME;
 export const DEFAULT_WINDOW_URL    = 'http://localhost:3000';
 export const DEFAULT_WINDOW_HEIGHT =  800;
@@ -60,14 +66,20 @@ export const TEMPLATES = [
 export default {
   
   app: {
-    name : 'SkullfaceApp',
+    icon : DEFAULT_APP_ICON,
+    name : DEFAULT_APP_NAME,
+    dir  : {
+      assets   : DEFAULT_APP_DIR_ASSETS,
+      backend  : DEFAULT_APP_DIR_BACKEND,
+      frontend : DEFAULT_APP_DIR_FRONTEND,
+    }
   },
   
   window: {
-    title  : 'SkullfaceApp',
-    url    : 'http://localhost:3000',
-    height : 768,
-    width  : 1024,
+    title  : DEFAULT_WINDOW_TITLE,
+    url    : DEFAULT_WINDOW_URL,
+    height : DEFAULT_WINDOW_HEIGHT,
+    width  : DEFAULT_WINDOW_WIDTH,
   },
 
   skullface: {
