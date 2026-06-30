9628d05817b702b9eecb7177ed2d2fe84289e855
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-06-30 14:34:26 +0200

    Create defaults.js

---
+++ packages/core/defaults.js
@@ -0,0 +1,16 @@
+// @skullface/core/defaults.js
+
+export default {
+  
+  app: {
+    name : 'SkullfaceApp',
+  },
+  
+  window: {
+    title  : 'SkullfaceApp',
+    url    : 'http://localhost:3000',
+    height : 768,
+    width  : 1024,
+  },
+
+}
