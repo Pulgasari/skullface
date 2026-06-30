fcd00308b4fe38ad7d4dcce029f864a780ea522f
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-06-30 18:56:28 +0200

    Create mod.js

---
+++ packages/cli/utils/mod.js
@@ -0,0 +1,6 @@
+// @skullface/cli/utils/mod.js
+
+export { default as applyTemplateVars } from './applyTemplateVars.ts';
+export { default as copyTemplate      } from './copyTemplate.ts';
+export { default as createContext     } from './createContext.ts';
+export { default as loadConfig        } from './loadConfig.ts';
