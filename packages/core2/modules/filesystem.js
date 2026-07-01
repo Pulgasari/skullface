09a4dec28d62b907b14ca34979fe4813c0030245
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-07-01 22:16:23 +0200

    Update filesystem.js

--- packages/core2/modules/filesystem.js
+++ packages/core2/modules/filesystem.js
@@ -2,55 +2,64 @@
 
 // :::::: API
 
-export const api = {
-  async readText (path) {
-    return await Deno.readTextFile(path);
-  },
+export async function readText (path) {
+  return await Deno.readTextFile(path);
+}
 
-  async writeText (path, text) {
-    await Deno.writeTextFile(path, text);
-  },
+export async function writeText (path, text) {
+  await Deno.writeTextFile(path, text);
+}
 
-  async readJSON (path) {
-    const text = await Deno.readTextFile(path);
-    return JSON.parse(text);
-  },
+export async function readJSON (path) {
+  const text = await Deno.readTextFile(path);
+  return JSON.parse(text);
+}
 
-  async writeJSON (path, obj) {
-    const text = JSON.stringify(obj, null, 2);
-    await Deno.writeTextFile(path, text);
-  },
+export async function writeJSON (path, obj) {
+  const text = JSON.stringify(obj, null, 2);
+  await Deno.writeTextFile(path, text);
+}
 
-  async exists (path) {
-    try        { await Deno.stat(path); return true; }
-    catch (_e) { return false; }
-  },
+export async function exists (path) {
+  try        { await Deno.stat(path); return true; }
+  catch (_e) { return false; }
+}
 
-  async copy (src, dest) {
-    await Deno.copyFile(src, dest);
-  },
+export async function copy (src, dest) {
+  await Deno.copyFile(src, dest);
+},
 
-  async remove (path) {
-    await Deno.remove(path, { recursive: true });
-  },
+export async function remove (path) {
+  await Deno.remove(path, { recursive: true });
+},
 
-  async mkdir (path) {
-    await Deno.mkdir(path, { recursive: true });
-  },
+export async function mkdir (path) {
+  await Deno.mkdir(path, { recursive: true });
+},
 
-  async walk (path, _options = {}) {
-    const results = [];
-    for await (const entry of Deno.readDir(path)) {
-      results.push({
-        isDirectory : entry.isDirectory,
-        isFile      : entry.isFile,
-        isSymlink   : entry.isSymlink,
-        name        : entry.name,
-      });
-    }
-    return results;
+export async function walk (path, _options = {}) {
+  const results = [];
+  for await (const entry of Deno.readDir(path)) {
+    results.push({
+      isDirectory : entry.isDirectory,
+      isFile      : entry.isFile,
+      isSymlink   : entry.isSymlink,
+      name        : entry.name,
+    });
   }
-  
-};
+  return results;
+}
 
-export default api;
+// :::::: DEFAULT
+
+export default {
+  copy,
+  exists,
+  mkdir,
+  readJSON,
+  readText,
+  remove,
+  walk,
+  writeJSON,
+  writeText,
+}
