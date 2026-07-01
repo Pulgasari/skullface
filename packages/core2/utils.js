8d2b57b7838372ecddaee4030744ce9eb7ec12d6
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-07-01 21:53:25 +0200

    Update utils.js

--- packages/core2/utils.js
+++ packages/core2/utils.js
@@ -1,3 +1,20 @@
 // @skullface/core/utils.js
 
+// :::::: CHECKS (IS)
+
 export const isFn = sth => typeof sth === 'function';
+
+// :::::: RUNTIME
+
+function getPlatform () {
+  const name = Deno.build.os;
+  return {
+    android : 'android',
+    darwin  : 'mac',
+    freebsd : 'freebsd',
+    linux   : 'linux',
+    mac     : 'mac',
+    macos   : 'mac',
+    windows : 'windows',
+  }[name];
+}
