c3a9a85887664c2dc6ce48d941cf111483c2c4bb
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-07-01 20:33:33 +0200

    Update client.js

--- packages/core2/client.js
+++ packages/core2/client.js
@@ -34,3 +34,26 @@ export function createStore (name) {
     update  : async (data)       => await ipc.update (name, data),
   };
 }
+
+/*
+
+export async function copy (text) {
+  await navigator.clipboard.writeText(text);
+}
+
+export async function paste () {
+  return await navigator.clipboard.readText();
+}
+
+export async function copyHTML (html) {
+  const blob = new Blob([html], { type: 'text/html' });
+  const item = new ClipboardItem({ 'text/html': blob });
+  await navigator.clipboard.write([item]);
+}
+
+export async function copyJSON (obj) {
+  const json = JSON.stringify(obj, null, 2);
+  await navigator.clipboard.writeText(json);
+}
+
+*/
