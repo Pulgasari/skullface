3208686e8c70e17ea2a7255b33aebd18e5919bab
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-07-02 06:02:54 +0200

    Update index.js

--- packages/client/index.js
+++ packages/client/index.js
@@ -3,9 +3,11 @@
 import { default as database } from './database.js';
 import { default as hotkeys  } from './hotkeys.js';
 import { default as router   } from './router.js';
+import { default as store    } from './store.js';
 
 export default {
   database,
   hotkeys,
   router,
+  store,
 }
