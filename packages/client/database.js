8e8b1613e0346cea3712433b9a32541143698acc
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-07-02 04:14:30 +0200

    Update database.js

--- packages/client/database.js
+++ packages/client/database.js
@@ -4,10 +4,12 @@ export const database = name => {
   const sqliteIPC = window.skullface.sqlite; // dynamic IPC-proxy
   return {
     execute: async (statement, values = []) => {
-      await sqliteIPC.execute(name, statement, values);
+      //await window.skullface.sqlite.execute(name, statement, values);
+      await window.skullface.ipc('database.execute', { name, statement, values })
     },
     query: async (statement, values = []) => {
-      return await sqliteIPC.query(name, statement, values);
+      //return await sqliteIPC.query(name, statement, values);
+      return await window.skullface.ipc('database.query', { name, statement, values });
     }
   };
 };
