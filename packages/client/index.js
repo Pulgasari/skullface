bc2068d71e63c6d1f704053835652ea7df0ff689
Pulgasari <106181130+Pulgasari@users.noreply.github.com>
2026-07-02 04:02:13 +0200

    Create index.js

---
+++ packages/client/index.js
@@ -0,0 +1,11 @@
+// @skullface/client/index.js
+
+import { default as database } from './database.js';
+import { default as hotkeys  } from './hotkeys.js';
+import { default as router   } from './router.js';
+
+export default {
+  database,
+  hotkeys,
+  router,
+}
